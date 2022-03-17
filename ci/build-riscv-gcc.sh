e8726bc967fb9c7ae36b070fc163f2dc207adae2
Daniel J.Mazure <daniel.jimenez2@bsc.es>
2022-03-17 03:00:04 +0100

    Update RISCV toolchain commit

--- ci/build-riscv-gcc.sh
+++ ci/build-riscv-gcc.sh
@@ -3,7 +3,7 @@
 
 set -e
 ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
-VERSION="691e4e826251c7ec59f883cab18440c87baf45e7"
+VERSION="051b9f7ddb7d136777505ea19c70a41926842b96"
 
 mkdir -p $RISCV
 
@@ -16,12 +16,12 @@ fi
 
 if ! [ -e $RISCV/bin ]; then
     if ! [ -e $RISCV/riscv-gnu-toolchain ]; then
-        git clone https://github.com/riscv/riscv-gnu-toolchain.git
+        git clone https://github.com/riscv-collab/riscv-gnu-toolchain.git
     fi
 
     cd riscv-gnu-toolchain
     git checkout $VERSION
-    git submodule update --init --recursive
+    #git submodule update --init --recursive
 
     if [[ $1 -ne "0" || -z ${1} ]]; then
       echo "Compiling RISC-V Toolchain"
